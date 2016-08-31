6308518bcd07cb8141d958ec5458f2d6fb5b3b48
Muream <muream@gmail.com>
2016-08-31 23:48:19 +0200

    initial commit

---
+++ downloadUtils.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib
+import os
+
+
+def gfycat_mp4(url, subreddit, index):
+    print "Downloading..."
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    directory = os.path.join(os.path.dirname(__file__), 'Downloads/{}'.format(subreddit))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # finds all the .mp4 files in the page
+    for source in soup.find_all('source', {"id": "mp4Source"}):
+        fileUrl = source['src']
+
+        fileName = subreddit + '_{0:0>3}'.format(index+1) + '.' + fileUrl.split('.')[-1]
+        urllib.urlretrieve(fileUrl, directory + '/' + fileName)
+
+
+if __name__ == '__main__':
+    gfycat_download('https://gfycat.com/FarawayLinedDouglasfirbarkbeetle', 'overwatch')
--- r_editor.py
+++ r_editor.py
@@ -1,37 +1,44 @@
 import praw
-import urllib
-import wget
-import urllib2
+import downloadUtils
+import sys
 
 
 USERAGENT = "(r)editor v1.0 by /u/Muream"
-MAXPOSTS = 5
+limitPosts = 100
 
-r = praw.Reddit(user_agent = USERAGENT)
+r = praw.Reddit(user_agent=USERAGENT)
 
-def run_bot(subreddit):
+
+def run_bot(subreddit, maxPosts):
     subreddit = r.get_subreddit(subreddit)
-    posts = subreddit.get_top_from_all(limit=MAXPOSTS)
-    i=1
+    posts = subreddit.get_top_from_all(limit=limitPosts)
+    linkPosts = []
+
+    # Flush the text posts
     for post in posts:
-        print "post {0:0>3}: ".format(i) + str(post.title)
+        if not post.is_self:
+            linkPosts.append(post)
+
+    # loop through the link posts
+    for index, post in enumerate(linkPosts):
+        print type(post.title)
+        print "post {0:0>3}: ".format(index + 1) + str(post.title)
         print "     url: {} ".format(str(post.url))
         print "  author: /u/{} ".format(str(post.author))
+
+        # if link is a gfycat link : Download the mp4
+        # if 'gfycat' in post.url:
+        #     downloadUtils.gfycat_mp4(post.url, str(subreddit), index)
+
+        if index + 1 > maxPosts:
+            break
         print
 
-        url = post.url
-        fileName = url.split('/')[-1]
-        urllib.urlretrieve (url, fileName)
 
-
-        i += 1
-
-# Main method
 def main():
     subreddit = raw_input("Subreddit: ")
     print
-    run_bot(subreddit)
+    run_bot(subreddit, 100)
 
 if __name__ == '__main__':
     main()
-
