2f4c369ccf7782267aee8cd44b7a61da83219166
Muream <muream@gmail.com>
2016-09-02 00:36:22 +0200

    imgur support

--- downloadUtils.py
+++ downloadUtils.py
@@ -4,13 +4,18 @@ import urllib
 import os
 
 
-def gfycat_mp4(url, subreddit, index):
-    print "Downloading..."
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
+def check_dir(subreddit):
     directory = os.path.join(os.path.dirname(__file__), 'Downloads/{}'.format(subreddit))
     if not os.path.exists(directory):
         os.makedirs(directory)
+    return directory
+
+
+def gfycat_mp4(url, subreddit, index):
+    print "Downloading from gfycat..."
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    directory = check_dir(subreddit)
 
     # finds all the .mp4 files in the page
     for source in soup.find_all('source', {"id": "mp4Source"}):
@@ -19,6 +24,23 @@ def gfycat_mp4(url, subreddit, index):
         fileName = subreddit + '_{0:0>3}'.format(index + 1) + '.' + fileUrl.split('.')[-1]
         urllib.urlretrieve(fileUrl, directory + '/' + fileName)
 
+    return directory
+
+
+def imgur_mp4(url, subreddit, index):
+    # find <source src="//i.imgur.com/G6j4BBF.mp4" type="video/mp4">
+    print "Downloading from imgur..."
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    directory = check_dir(subreddit)
+
+    for source in soup.find_all('source', {"type": "video/mp4"}):
+        fileUrl = "https:{}".format(source['src'])
+        print fileUrl
+        fileName = subreddit + '_{0:0>3}'.format(index + 1) + '.' + fileUrl.split('.')[-1]
+        urllib.urlretrieve(fileUrl, directory + '/' + fileName)
+
+    return directory
 
 if __name__ == '__main__':
-    gfycat_download('https://gfycat.com/FarawayLinedDouglasfirbarkbeetle', 'overwatch')
+    imgur_mp4('http://i.imgur.com/G6j4BBF.gifv', 'overwatch', 1)
--- r_editor.py
+++ r_editor.py
@@ -9,11 +9,22 @@ limitPosts = 100
 r = praw.Reddit(user_agent=USERAGENT)
 
 
+class clip(praw.objects.Submission):
+    """
+    The clip class, inherited from the Submission class of Praw (not sure if it's the right one)
+    """
+    def __init__(self, path):
+        self.path = path
+
+    def download_video(self):
+
+
+
 def run_bot(subreddit, maxPosts):
     subreddit = r.get_subreddit(subreddit)
     posts = subreddit.get_top_from_all(limit=limitPosts)
     linkPosts = []
-
+    clips = []
     # Flush the text posts
     for post in posts:
         if not post.is_self:
@@ -22,7 +33,7 @@ def run_bot(subreddit, maxPosts):
     # loop through the link posts
     for index, post in enumerate(linkPosts):
 
-        # FIXME:some title have encoding problems
+        # FIXME: Some titles seem to have encoding problems
         # print type(post.title)
         # name = "post {0:0>3}: ".format(index + 1) + str(post.title)
         print "     url: {} ".format(str(post.url))
@@ -31,19 +42,22 @@ def run_bot(subreddit, maxPosts):
         # if link is a gfycat link : Download the mp4
         if 'gfycat' in post.url:
             print "gfycat link"
-            downloadUtils.gfycat_mp4(post.url, str(subreddit), index)
+            directory = downloadUtils.gfycat_mp4(post.url, str(subreddit), index)
         elif 'imgur' in post.url:
             print "imgur link"
+            directory = downloadUtils.imgur_mp4(post.url, str(subreddit), index)
 
         if index + 1 > maxPosts:
             break
         print
 
+        return clips
+
 
 def main():
     subreddit = raw_input("Subreddit: ")
     print
-    run_bot(subreddit, 100)
+    run_bot(subreddit, 5)
 
 if __name__ == '__main__':
     main()
