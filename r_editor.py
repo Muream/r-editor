c4672f87223e990233b2437624fedf670849a6a7
Muream <muream@gmail.com>
2016-09-02 22:51:21 +0200

    does the edit!

--- r_editor.py
+++ r_editor.py
@@ -1,52 +1,63 @@
-import praw
 import utils
 import sys
+from moviepy.editor import *
 
 
-# class clip(praw.objects.Submission):
-#     """
-#     The clip class, inherited from the Submission class of Praw
-#     (not sure if it's the right one)
-#     """
-#     def __init__(self, path):
-#         self.path = path
+class clip:
+    """
+    Simple clip class to get stuff organized
+    """
+
+    def __init__(self, title, url, author, index, path):
+        self.title = title
+        self.url = url
+        self.author = author
+        self.index = index
+        self.path = path
 
 
-def run_bot(subreddit, maxPosts):
+def get_clips(subreddit, maxPosts):
     posts = utils.get_posts(subreddit, maxPosts)
     clips = []
 
-    # loop through the link posts
+    # loop through the link posts and downloads from gfycat or imgur
     for index, post in enumerate(posts):
 
-        # FIXME: Some titles seem to have encoding problems
-        # print type(post.title)
-        # name = "post {0:0>3}: ".format(index + 1) + str(post.title)
+        print
+        print "retrieving video #{0:0>3}".format(index + 1)
+        if 'gfycat' in post.url:
+            path = utils.gfycat_mp4(post.url, str(subreddit), index)
+        elif 'imgur' in post.url:
+            path = utils.imgur_mp4(post.url, str(subreddit), index)
 
-        print index + 1
-        print "url: {} ".format(str(post.url))
-        print "author: /u/{} ".format(str(post.author))
-
-        # if link is a gfycat link : Download the mp4
-        # if 'gfycat' in post.url:
-        #     print "gfycat link"
-        #     directory = downloadUtils.gfycat_mp4(post.url, str(subreddit), index)
-        # elif 'imgur' in post.url:
-        #     print "imgur link"
-        #     directory = downloadUtils.imgur_mp4(post.url, str(subreddit), index)
+        currentClip = clip(post.title, post.url, post.author, index + 1, path)
+        clips.append(currentClip)
 
         if index + 1 >= maxPosts:
-            print "break"
+            print "All {} clips have been downloaded!\n".format(maxPosts)
             break
         print
 
     return clips
 
 
+def edit_clips(clips, subreddit, maxPosts):
+    paths = []
+    for clip in clips:
+        currClip = VideoFileClip(clip.path)
+        currClip = vfx.resize(currClip, width=1280, height=720)
+        paths.append(currClip)
+
+    finalClip = concatenate_videoclips(paths)
+    finalClip.write_videofile('Exports/{}_top{}.mp4'.format(subreddit, maxPosts), fps=24)
+
+
 def main():
     subreddit = raw_input("Subreddit: ")
+    maxPosts = 5
     print
-    run_bot(subreddit, 5)
+    clips = get_clips(subreddit, maxPosts)
+    edit_clips(clips, subreddit, maxPosts)
 
 if __name__ == '__main__':
     main()
